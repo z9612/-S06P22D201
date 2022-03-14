6b85b2ff05230939f91ec1e87917a91fae1c0695
elenaisnanocat <elenassafy0201@gmail.com>
2022-03-14 11:18:44 +0900

    [FE-박유정][reactpage-21] feat: 리액트 기본 페이지 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import {ErrorBoundary} from 'react-error-boundary';
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import MainPage from "./components/views/MainPage/MainPage";
+import NotFound from "./components/views/NotFound/NotFound";
+import Error from "./components/views/Error/Error";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary FallbackComponent={<Error />}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/main" element={<MainPage />} />
+          <Route path={"*"} element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
