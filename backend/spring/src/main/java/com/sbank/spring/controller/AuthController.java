4e54106f494d464681319946741de73cdf14599c
박현진 <jinnydiary@naver.com>
2022-03-17 14:06:46 +0900

    [BE-박현진][signup-29] feat: 아이디&이메일 중복검사 구현

--- backend/spring/src/main/java/com/sbank/spring/controller/AuthController.java
+++ backend/spring/src/main/java/com/sbank/spring/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.sbank.spring.controller;
 
+import javax.validation.Valid;
+
 import com.sbank.spring.dto.MemberDto;
 import com.sbank.spring.dto.SigninDto;
 import com.sbank.spring.dto.TokenDto;
@@ -10,12 +12,14 @@ import com.sbank.spring.service.MemberService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -29,18 +33,27 @@ public class AuthController {
     private final AuthService authService;
     private final MemberService mamberService;
     
-    @GetMapping("/hello") //Test API
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("hello");
-    }
-    
+    @Operation(summary="로그인")
     @PostMapping("/signin")
-    public ResponseEntity<TokenDto> signin(@RequestBody SigninDto signinDto) {
+    public ResponseEntity<TokenDto> signin(@Valid @RequestBody SigninDto signinDto) {
         return ResponseEntity.ok(authService.signin(signinDto));
     }
 
+    @Operation(summary="회원가입")
     @PostMapping("/signup")
-    public ResponseEntity<Member> signup(@RequestBody MemberDto memberDto) {
+    public ResponseEntity<Member> signup(@Valid @RequestBody MemberDto memberDto) {
         return ResponseEntity.ok(mamberService.signup(memberDto));
+    }
+
+    @Operation(summary="아이디 중복 검사", description="있는 아이디라면 true, 없는 아이디라면 false 리턴")
+    @GetMapping("/duplicate/id/{id}")
+    public ResponseEntity<Boolean> duplicateId(@PathVariable String id) {
+        return ResponseEntity.ok(mamberService.duplicateId(id));
+    }
+
+    @Operation(summary="이메일 중복 검사", description="있는 이메일라면 true, 없는 이메일라면 false 리턴")
+    @GetMapping("/duplicate/email/{email}")
+    public ResponseEntity<Boolean> duplicateEmail(@PathVariable String email) {
+        return ResponseEntity.ok(mamberService.duplicateEmail(email));
     }
 }
