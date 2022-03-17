67b0496c8e1da1cb6a4f681c168449a6b8cc5a86
박현진 <jinnydiary@naver.com>
2022-03-17 14:05:04 +0900

    [BE-박현진][signup-29] perf: 작성 형식 및 데이터 타입 변경

--- backend/spring/src/main/java/com/sbank/spring/dto/SigninDto.java
+++ backend/spring/src/main/java/com/sbank/spring/dto/SigninDto.java
@@ -1,5 +1,7 @@
 package com.sbank.spring.dto;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +14,8 @@ import lombok.Setter;
 @AllArgsConstructor //모든 필드 값을 파라미터로 받는 생성자 생성
 @NoArgsConstructor //파라미터 없는 기본 생성자 생성
 public class SigninDto {
+    @NotNull
     private String id;
+    @NotNull
     private String password;
 }
