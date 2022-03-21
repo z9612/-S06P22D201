b7d95e41767093c0b4df707dc3dffde593b7444f
최기운 <z961223@gmail.com>
2022-03-21 04:33:44 +0000

    [BE-최기운][deploy-68] fix: swagger import 수정

--- backend/spring/src/main/java/com/sbank/spring/controller/TestController.java
+++ backend/spring/src/main/java/com/sbank/spring/controller/TestController.java
@@ -5,22 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 @RestController
 @RequestMapping("/api")
-@Api("Test Controller API")
 public class TestController {
 
-    @ApiOperation(value = "hello 출력")
     @GetMapping("/hello1")
     public String hello1() {
         return "hello";
     }
 
-    @ApiOperation(value = "입력 값 출력", notes = "입력 받은 값을 그대로 반환합니다.", response = String.class)
     @GetMapping("/hello2")
     public String hello2(@RequestParam String param) {
         return param;
