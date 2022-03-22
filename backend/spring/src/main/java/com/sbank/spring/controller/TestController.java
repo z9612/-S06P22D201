815a80c18276fd360398125f3a0a8fcbcbaa08ef
박현진 <jinnydiary@naver.com>
2022-03-22 11:56:29 +0900

    [BE-박현진][account-57] fix: 사용 안하는 annotation 삭제

--- backend/spring/src/main/java/com/sbank/spring/controller/TestController.java
+++ backend/spring/src/main/java/com/sbank/spring/controller/TestController.java
@@ -5,10 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-
 @RestController
 @RequestMapping("/api")
 public class TestController {
